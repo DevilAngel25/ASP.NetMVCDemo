381d30a56205e75b7c9055af9eb40d96e1f7fb2f
Lloyd Fitzroy-Smith <lloydfitzroysmith@googlemail.com>
2019-02-28 16:36:59 +0000

    Add project files.

---
+++ ASP.NetMVCDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP.NetMVCDemo.Startup))]
+namespace ASP.NetMVCDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
